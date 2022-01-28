68e2d0f34df16e46b5423dd25a19c09bdb231cc4
Piyush Chamoli <piyushchamoli11@gmail.com>
2022-01-28 20:20:48 +0530

    update index.js

--- block-BJaaju/code/index.js
+++ block-BJaaju/code/index.js
@@ -2,3 +2,46 @@
 // https://api.unsplash.com/
 // /photos/random
 // /search/photos
+
+const input = document.querySelector('input');
+const root = document.querySelector('.images');
+
+const url = `https://api.unsplash.com/photos/?client_id=c4dgBRN_iDTy-gevrd_vDULPy_EFNfKcEZAjk1d5fPI`
+
+const getSearchUrl = (query) => `https://api.unsplash.com/search/photos?query=${query}&client_id=c4dgBRN_iDTy-gevrd_vDULPy_EFNfKcEZAjk1d5fPI`;
+
+function fetch (url, successHandler) {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.onload = () => successHandler(JSON.parse(xhr.response))
+    xhr.onerror = function () {
+        console.log('Something Went Wrong...');
+    } 
+    xhr.send();
+}
+
+function displayImages (images) {
+    root.innerHTML = '';
+    images.forEach(image => {
+        let li = document.createElement('li');
+        let img = document.createElement('img');
+        img.src = image.urls.thumb;
+        li.append(img);
+        root.append(li);
+    });
+}
+
+fetch(url,displayImages);
+
+function handleSearch (event) {
+    if (event.keyCode === 13 && input.value) {
+        console.log(input.value);
+        fetch(getSearchUrl(input.value), (searchResult) => {
+            displayImages(searchResult.results);
+        });
+        input.value = '';
+    }
+}
+
+input.addEventListener('keyup', handleSearch);
+
